1653d994478e84d8971754a31d751ce250f2ea48
alu0100785265 <alu0100785265@ull.edu.es>
2017-02-12 13:30:16 +0000

    añadiendo página personal

---
+++ js/json/dsi.js
@@ -0,0 +1,12 @@
+{
+    "Asignatura": "Desarrollo de Sistemas Informáticos",
+    "practicas": [
+        {
+            "id" : 1,
+            "titulo": "Tareas Iniciales",
+            "imagen": "img/practicas/dsi/practi1.png",
+            "url": "https://github.com/ULL-ESIT-DSI-1617/tareas-iniciales-jairo-lucas-ivan",
+            "url_paginaweb": "http://ULL-ESIT-DSI-1617.github.io/tareas-iniciales-jairo-lucas-ivan/"
+        }
+    ]
+}
