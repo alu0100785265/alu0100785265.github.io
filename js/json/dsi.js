94cda4765fd9df2d9e8c183c5ed0d5bdb5292491
alu0100785265 <alu0100785265@ull.edu.es>
2017-02-15 14:34:25 +0000

    añadiendo cambios

--- js/json/dsi.js
+++ js/json/dsi.js
@@ -7,6 +7,13 @@
             "imagen": "img/practicas/dsi/practi1.png",
             "url": "https://github.com/ULL-ESIT-DSI-1617/tareas-iniciales-jairo-lucas-ivan",
             "url_paginaweb": "http://ULL-ESIT-DSI-1617.github.io/tareas-iniciales-jairo-lucas-ivan/"
+        },
+         {
+            "id" : 2,
+            "titulo": "Tareas Iniciales",
+            "imagen": "img/practicas/dsi/practi2.png",
+            "url": "https://github.com/ULL-ESIT-DSI-1617/primeros-pasos-en-nodejs-jairo-lucas-miguel-ivan.git",
+            "url_paginaweb": "http://ULL-ESIT-DSI-1617.github.io/primeros-pasos-en-nodejs-jairo-lucas-miguel-ivan/"
         }
     ]
 }
